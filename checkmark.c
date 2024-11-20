df474bf382ddf26209f8dc413063362971dde62e
Saixi <elvirdelic.ed@gmail.com>
2024-11-20 03:12:43 -0500

    Create checkmark.c

---
+++ checkmark.c
@@ -0,0 +1,14 @@
+// a program that uses printf to display a checkmark
+#include <stdio.h>
+
+int main(void) {
+ 
+printf("       *\n");
+printf("      *\n");
+printf("     *\n");
+printf("*   *\n");
+printf(" * *\n");
+printf("  *");
+
+	return 0;
+}
